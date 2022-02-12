92770b1f19404c14d460cc704e0db39b7a21ce17
Sebastian Lopez Herrera <sebaslhdev@gmail.com>
2022-02-12 02:01:24 -0500

    Added Streams exercises

---
+++ java-streams/src/test/java/com/amigoscode/examples/Filtering.java
@@ -0,0 +1,69 @@
+package com.amigoscode.examples;
+
+import com.amigoscode.beans.Car;
+import com.amigoscode.beans.Person;
+import com.amigoscode.mockdata.MockData;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Filtering {
+
+    @Test
+    public void filter() throws Exception {
+        List<Car> cars = MockData.getCars();
+        List<Car> carLessThank20k = cars.stream()
+                .filter(car -> car.getPrice() < 20_000.00)
+                .collect(Collectors.toList());
+        carLessThank20k.forEach(System.out::println);
+    }
+
+    @Test
+    public void dropWhile() throws Exception {
+        System.out.println("using filter");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
+        System.out.println();
+        System.out.println("using dropWhile");
+    }
+
+    @Test
+    public void takeWhile() throws Exception {
+        // using filter
+        System.out.println("using filter");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
+
+        System.out.println();
+        System.out.println("using take while");
+    }
+
+    @Test
+    public void findFirst() throws Exception {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    }
+
+    @Test
+    public void findAny() throws Exception {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10};
+    }
+
+    @Test
+    public void allMatch() throws Exception {
+        int[] even = {2, 4, 6, 8, 10};
+    }
+
+    @Test
+    public void anyMatch() throws Exception {
+        int[] evenAndOneOdd = {2, 4, 6, 8, 10, 11};
+    }
+
+}
+
+
+
