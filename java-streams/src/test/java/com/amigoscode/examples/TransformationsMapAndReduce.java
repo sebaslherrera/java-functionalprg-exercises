a0971b281b43fbf626f760b466b6a56873f8a03e
Sebastian Lopez H <sebaslhdev@gmail.com>
2022-02-13 22:04:39 -0500

    Understanding map and mapToDouble

--- java-streams/src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
+++ java-streams/src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -17,11 +18,23 @@ public class TransformationsMapAndReduce {
     @Test
     void yourFirstTransformationWithMap() throws IOException {
         List<Person> people = MockData.getPeople();
+        Function<Person, PersonDTO> personPersonDTOFunction = person ->
+            new PersonDTO(person.getId(), person.getFirstName(), person.getAge());
+
+        //Call method reference, for more info go to map static method
+        Function<Person, PersonDTO> mapper = PersonDTO::map;
+        List<PersonDTO> personDTOS = people.stream().map(mapper).collect(Collectors.toList());
+        personDTOS.forEach(System.out::println);
     }
 
     @Test
     void mapToDoubleAndFindAverageCarPrice() throws IOException {
         List<Car> cars = MockData.getCars();
+
+        double v = cars.stream()
+                .mapToDouble(car -> car.getPrice())
+                .average().orElse(0);
+        System.out.println(v);
     }
 
     @Test
